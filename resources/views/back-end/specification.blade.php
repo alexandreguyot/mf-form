308e66467b0f38d598de1d789b694d8cad607267
Guillaume Sourisseau <spk@macbook-pro-de-guillaume.home>
2018-04-22 23:35:56 +0200

    Integration formulaire HTML

--- resources/views/back-end/specification.blade.php
+++ resources/views/back-end/specification.blade.php
@@ -3,9 +3,8 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">
+            <div class="card-header text-center">
                     Création du cahier des charges
-
             </div>
             <div class="card-body">
                 @include('form')
