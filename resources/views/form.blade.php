b35f58cad22b35c84d975830d26dab5e24c3bc50
Guillaume Sourisseau <sourisseauguillaume@gmail.com>
2018-04-23 19:26:20 +0200

    Integration

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,4 +1,4 @@
-{{--<script src="{{ asset('js/application.js') }}" defer></script>--}}
+<script src="{{ asset('js/application.js') }}" defer></script>
 
 <form id="cahier_charges" method="POST" action="{{ route('create_specification') }}">
     <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
